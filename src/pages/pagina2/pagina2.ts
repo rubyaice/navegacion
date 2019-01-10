49b0f03a257189578d1a7ac9d3c4c9e9ca5361b1
rubyaice <andrex.silva.777@gmail.com>
2019-01-09 21:01:29 -0430

    Navegacion entre paginas, envio de datos entre ts y por html
    Navegacion entre paginas, envio de datos entre ts y por html

--- src/pages/pagina2/pagina2.ts
+++ src/pages/pagina2/pagina2.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the Pagina2Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Pagina3Page } from "../index.paginas";
 
 @IonicPage()
 @Component({
@@ -15,11 +10,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Pagina2Page {
 
+  pagina3:any = Pagina3Page;
+
+  mutantes: any[] = [
+
+    {
+      nombre: "Magneto",
+      poder: "controlar metal"
+    },
+
+    {
+      nombre: "Profesor X",
+      poder: "Poderes Psiquicos"
+    },
+
+    {
+      nombre: "Ciclope",
+      poder: "Lanzar rayos desde los ojos ... o sea"
+    },
+
+    {
+      nombre: "Gambito",
+      poder: "Cargar objetos inanimados con energia"
+    }
+
+  ];
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Pagina2Page');
+  irPagina3( mutante:any ){
+
+    console.log(mutante);
+    this.navCtrl.push( Pagina3Page, { 'mutante' : mutante } );
   }
+
 
 }
--- src/pages/pagina3/pagina3.ts
+++ src/pages/pagina3/pagina3.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the Pagina3Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-pagina3',
@@ -15,11 +8,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Pagina3Page {
 
+  mutante:any = {};
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    console.log(navParams);
+
+    this.mutante = this.navParams.get('mutante');
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Pagina3Page');
-  }
 
 }
