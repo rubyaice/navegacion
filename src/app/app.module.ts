f2993e7155494e142355ccf2a96caf881f8fc74f
rubyaice <andrex.silva.777@gmail.com>
2019-01-09 19:51:46 -0430

    Agregado de paginas y tabs, ts index
    Agregado de paginas y tabs, tambien se agrego el ts index.paginas que contiene todas las paginas y solo hay que llamarlas de ahi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { PrincipalPage, AjustesPage, Ajustes2Page, Pagina2Page, Pagina3Page, ModalPage, TabsPage } from "../pages/index.paginas";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    PrincipalPage,
+    AjustesPage,
+    Ajustes2Page,
+    Pagina2Page,
+    Pagina3Page,
+    ModalPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +26,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    PrincipalPage,
+    AjustesPage,
+    Ajustes2Page,
+    Pagina2Page,
+    Pagina3Page,
+    ModalPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
